8bea51fc795d7f89e6c07abf9670473ce0b0d70f
KristinaMG <kristina_gundersen@msn.com>
2014-03-20 06:22:31 +0100

    Sqldatasource med delete og update

---
+++ Sqldatasource eksempel/TeamAdministrasjon.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Timeregistreringssystem
+{
+    public partial class TeamAdministrasjon : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gridViewTeam_SelectedIndexChanged(object sender, EventArgs e) { }
+
+        protected void btnNyttTeam_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("OpprettTeam.aspx");
+        }
+
+        protected void gridViewTeam_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            //Sjekk etter ugyldig input
+        }
+
+        protected void gridViewTeam_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+           
+        }
+    }
+}
